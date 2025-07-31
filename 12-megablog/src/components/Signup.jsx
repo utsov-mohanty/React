db5166699ea3c7b8cb9651cd3db58c36dee78f9f
Utsov Mohanty <2205952@kiit.ac.in>
2025-07-31 21:27:20 +0530

    components and form updated

--- 12-megablog/src/components/Signup.jsx
+++ 12-megablog/src/components/Signup.jsx
@@ -1,55 +1,60 @@
 import React, {useState} from 'react'
-import {Link, useNavigate} from 'react-router-dom'
-import {login as authLogin} from '../store/authSlice'
+import authService from '../appwrite/auth'
+import { Link, useNavigate } from 'react-router-dom'
+import {login} from '../store/authSlice'
 import {Button, Input, Logo} from './index'
 import { useDispatch } from 'react-redux'
-import authService from '../appwrite/auth'
-import {useForm} from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 
-function Login() {
+function Signup() {
     const navigate = useNavigate()
+    const [error, setError] = useState("")
     const dispatch = useDispatch()
     const {register, handleSubmit} = useForm()
-    const [error, setError] = useState("")
 
-    const login = async(data) => {
+    const create = async(data) => {
         setError("")
         try {
-            const session = await authService.login(data)
-            if(session){
+            const userData = await authService.createAccount(data)
+            if(userData){
                 const userData = await authService.getCurrentUser()
-                if(userData){
-                    dispatch(authLogin(userData))
-                }
-                navigate('/')
+                if(userData) dispatch(login(userData));
+                navigate("/")
             }
         } catch (error) {
             setError(error.message)
         }
     }
+
   return (
-    <div
-    className='flex items-center justify-center w-full'
-    >
+    <div className='flex items-center justify-center'>
         <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
             <div className='mb-2 flex justify-center'>
                 <span className='inline-block w-full max-w-[100px]'>
                     <Logo width='100%'/>
                 </span>
             </div>
-            <h2 className='text-center text-2xl font-bold leading-tight'>Sign in to your account</h2>
+            <h2 className='text-center text-2xl font-bold leading-tight'>Sign Up to create account</h2>
             <p className='mt-2 text-center text-base text-black/60'>
-            Don&apos;t have any account?&nbsp;
-            <Link
-                to="/signup"
+                Already have an account?&nbsp;
+                <Link
+                to="/login"
                 className='font-medium text-primary transition-all duration-200 hover:underline'
-            >
-                Sign Up
-            </Link>
+                >
+                    Sign In
+                </Link>
             </p>
-            {error && <p className='text-red-600 mt-8 text-center'>{error}</p>}
-            <form onSubmit={handleSubmit(login)} className='mt-8'>
+            {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+
+            <form onSubmit={handleSubmit(create)}>
                 <div className='space-y-5'>
+                    <Input 
+                    label= "Full Name: "
+                    placeholder="Enter your full name"
+                    {...register("name", {
+                        required: true,
+                    })}
+                    />
                     <Input 
                     label= "Email: "
                     placeholder="Enter your email"
@@ -57,11 +62,11 @@ function Login() {
                     {...register("email", {
                         required: true,
                         validate: {
-                            matchPattern: (value) => /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/.test(value) || "Email address must be a valid address",
+                        matchPattern: (value) => /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/.test(value) || "Email address must be a valid address",
                         }
                     })}
                     />
-                    <Input
+                    <Input 
                     label="Password: "
                     type="password"
                     placeholder="Enter your password"
@@ -72,7 +77,7 @@ function Login() {
                     <Button
                     type="submit"
                     className="w-full"
-                    >Sign In</Button>
+                    >Create Account</Button>
                 </div>
             </form>
         </div>
@@ -80,4 +85,4 @@ function Login() {
   )
 }
 
-export default Logince
+export default Signup
